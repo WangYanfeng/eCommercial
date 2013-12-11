3cd7e45531e383d003d4b2f60c843d6c3c30c70d
WangyfHYJY <wangyanfeng_2012@163.com>
2013-12-11 17:55:56 +0800

    test4 mysql

--- home/Lib/Action/IndexAction.class.php
+++ home/Lib/Action/IndexAction.class.php
@@ -18,6 +18,13 @@ class IndexAction extends Action {
             $this->error('密码错误');
         }
     	}else{
+        echo "用户名:" . $_ENV["JAE_MYSQL_USERNAME"] ."</br>";
+        echo "密码:" .$_ENV["JAE_MYSQL_PASSWORD"] . "</br>";
+        echo "IP:" . $_ENV["JAE_MYSQL_IP"] ."</br>";
+        echo "数据库编码:" .$_ENV["JAE_MYSQL_ENCODING"] . "</br>";
+        echo "端口:" .$_ENV["JAE_MYSQL_PORT"] . "</br>";
+        echo "数据库名称:" .$_ENV["JAE_MYSQL_DBNAME"] . "</br>";
+        die();
     		$this->display();
     	}
   }
